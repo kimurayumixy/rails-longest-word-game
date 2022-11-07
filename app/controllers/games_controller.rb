1ffeee7624526d8b2e2978251555e9948a3320f1
Yumi Kimura <kimurayumixy@gmail.com>
2022-11-07 16:31:45 +0900

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,14 +10,15 @@ class GamesController < ApplicationController
     @answer = params[:answer]
     url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
     hash = JSON.parse(URI.open(url).read)
-    included = (@answer.split("") - params[:letters].split(" ")).empty?
-
+    grid_arr = params["letters"].split(" ")
+    answer_arr = params["answer"].split("")
+    included = answer_arr.all? { |answer_letter| answer_arr.count(answer_letter) <= grid_arr.count(answer_letter) }
     if hash["found"] && included
       @result = "congrats"
-    elsif !hash["found"] && included
-      @result = "Not an English word!"
-    else
+    elsif hash["found"] && !included
       @result = "Not included in the grid!"
+    else
+      @result = "Not an English word!"
     end
   end
 end
